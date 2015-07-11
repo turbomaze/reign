ae999002cc65d36bca96aa707aaeb9b31e25413c
Anthony Liu <anthonyscottliu@gmail.com>
2015-07-11 03:32:02 -0700

    Adds a function that follows a random policy until termination.

--- js/reinforcementlearner.js
+++ js/reinforcementlearner.js
@@ -31,15 +31,16 @@ var ReinforcementLearner = (function() {
     var TRANSITION = function(s, a) {
         var tileType = GRID[s[0]][s[1]];
         if (tileType === 2 || tileType === 3) {
-            if (a === 4) return [[false, false], 1]; //guaranteed to exit
-            else return [s.slice(0), 1]; //same spot otherwise
+            if (a === 4) return [[[false, false], 1]]; //guaranteed to exit
+            else return [[s.slice(0), 1]]; //same spot otherwise
         } else {
             //0 deg shift clockwise, 90, 180, 270
             var probs = [
                 [0.8, 0.1, 0, 0.1], //up: up, right, down, left
                 [0.1, 0.8, 0.1, 0], //right: up, right, down, left
                 [0, 0.1, 0.8, 0.1], //down: up, right, down, left
-                [0.1, 0, 0.1, 0.8] //left: up, right, down, left
+                [0.1, 0, 0.1, 0.8], //left: up, right, down, left
+                [0, 0, 0, 0] //exit: up, right, down, left
             ];
             var trsns = [];
             if (s[0] > 0 && GRID[s[0]-1][s[1]] !== 1) {
@@ -82,6 +83,7 @@ var ReinforcementLearner = (function() {
         canvas.height = DIMS[1];
         ctx = canvas.getContext('2d');
 
+        //reinforcement learning stuff
         learner = new Reign(GRID, REWARD, ACTIONS, TRANSITION, [2, 0],
             function(state) {
                 clearCanvas(); //clean slate
@@ -89,9 +91,12 @@ var ReinforcementLearner = (function() {
                 paintAgent(state[0], state[1]); //draw the agent
             }
         );
+        learner.actUntilExit();
     }
 
     function paintAgent(c1, c2) {
+        if (c1 === false) return; //don't draw the agent if it's exited
+
         var yOff = (c1 + 0.5)*TILE_HT;
         var xOff = (c2 + 0.5)*TILE_WD;
         drawPoint([xOff, yOff], Math.min(TILE_WD, TILE_HT)/5, TILE_COLS[1]);
@@ -125,9 +130,9 @@ var ReinforcementLearner = (function() {
      * objects */
     function Reign(world, reward, actions, transition, initState, every) {
         //constants
-        this.world = world; //the geography of the world
+        this.world = world.slice(0); //the geography of the world
         this.reward = reward; //the values of each state
-        this.actions = actions; //the actions available
+        this.actions = actions.slice(0); //the actions available
         this.transition = transition; //the action probabilities
         this.initState = initState.slice(0); //state to restart in after exit
         this.every = every || function() {}; //run after each action
@@ -139,8 +144,23 @@ var ReinforcementLearner = (function() {
         this.every(this.state);
 
         //methods
+        this.actUntilExit = function() {
+            if (this.state[0] !== false) {
+                var action = this.actions[
+                    Math.floor(this.actions.length*Math.random())
+                ];
+                console.log(['up','right','down','left','EXIT'][action]);
+                this.takeAction(action);
+
+                setTimeout(this.actUntilExit.bind(this), 500);
+            }
+        };
         this.takeAction = function(a) {
-            this.every(state);
+            var possibleEndStates = this.transition(this.state, a);
+            var result = this.chooseRandomly(possibleEndStates);
+            this.state = result.slice(0);
+
+            this.every(this.state);
         };
         this.chooseRandomly = function(set) {
             //given a set of object-weight pairs, choose a random object
@@ -162,7 +182,7 @@ var ReinforcementLearner = (function() {
                 }
             }
             return set[0][0]; //unexpected error; return first one
-        }
+        };
     }
 
     /********************
