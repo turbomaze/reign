f3003ae41e949f88011b5a7bb6f663373a24f4c0
Anthony Liu <anthonyscottliu@gmail.com>
2015-07-11 01:19:03 -0700

    Draws the grid environment.

--- js/reinforcementlearner.js
+++ js/reinforcementlearner.js
@@ -12,10 +12,18 @@ var ReinforcementLearner = (function() {
 
     /**********
      * config */
-    var DIMS = [960, 500];
+    var DIMS = [800, 450];
+    var GRID = [
+        [0, 0, 0, 2],
+        [0, 1, 0, 3],
+        [0, 0, 0, 0]
+    ]; //tile types: {0: empty, 1: wall, 2: good exit, 3: bad exit}
 
     /*************
      * constants */
+    var TILE_WD = DIMS[0]/GRID[0].length;
+    var TILE_HT = DIMS[1]/GRID.length;
+    var TILE_COLS = ['#EFEFEF', '#424242', '#83E378', '#ED7C6F'];
 
     /*********************
      * working variables */
@@ -29,7 +37,32 @@ var ReinforcementLearner = (function() {
         canvas.width = DIMS[0];
         canvas.height = DIMS[1];
         ctx = canvas.getContext('2d');
-        clearCanvas('red');
+
+        //draw the grid
+        paintGrid();
+    }
+
+    function paintGrid() {
+        //color cells
+        for (var ai = 0; ai < GRID.length; ai++) {
+            var yOff = ai*TILE_HT;
+            for (var bi = 0; bi < GRID[0].length; bi++) {
+                var xOff = bi*TILE_WD;
+                ctx.fillStyle = TILE_COLS[GRID[ai][bi]];
+                ctx.fillRect(xOff, yOff, TILE_WD, TILE_HT);
+            }
+        }
+
+        //draw borders
+        ctx.fillStyle = '#101010';
+        for (var ai = 0; ai <= GRID.length; ai++) {
+            var yOff = ai*TILE_HT - (ai === GRID.length ? 1 : 0);
+            ctx.fillRect(0, yOff, canvas.width, 1);
+        }
+        for (var ai = 0; ai <= GRID[0].length; ai++) {
+            var xOff = ai*TILE_WD - (ai === GRID[0].length ? 1 : 0);
+            ctx.fillRect(xOff, 0, 1, canvas.height);
+        }
     }
 
     /***********
